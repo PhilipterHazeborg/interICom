0b05a4c855c37ef894aa31d7ba38289a2da2d069
sadcat <lemap28@gmail.com>
2023-02-19 20:51:17 +0100

    init

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def loop():
     while True:
         GPIO.output(2, True)
         print("up")
-        time.sleep(0.05)
+        time.sleep(0.02)
         GPIO.output(2, False)
         print("down")
-        time.sleep(0.05)
+        time.sleep(0.02)
 
 
 if __name__ == "__main__":
