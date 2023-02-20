606f7a29fd04ffce47c2de6cdcab17668816ba47
sadcat <lemap28@gmail.com>
2023-02-20 23:00:45 +0100

    tr with clock

--- esp/esp.ino
+++ esp/esp.ino
@@ -6,8 +6,10 @@ unsigned long timestamp;
 int clkState = 0;
 int lastclkState = 0;
 int counter = 0;
-int buffer_array[4];
+char* buffer_array[5];
 char str_buffer[4];
+char* new_str;
+char buffer_str;
 
 void setup() {
   setlocale(LC_ALL, "en_US.utf8");
@@ -18,35 +20,38 @@ void setup() {
 }
 
 void loop() {
+  //Serial.println(counter);
+  yield();
   BTN_state = digitalRead(BTN);
   clkState = digitalRead(clk);
   
-  if(counter == 3){
-    for(int i = 0; i = 3; i++){
-      str_buffer[i] = /*(String)*/buffer_array[i];
-      counter = 0;
-    }
-    Serial.println(str_buffer);
+  if(counter == 4){
+    counter = 0;
+    buffer_array[4] = "\0";
+    String s = buffer_array;
+    Serial.println(s);
+
+    Serial.println("-----");
   }
   
   if(clkState != lastclkState) {
     if(clkState == HIGH){
-      
+      /*
       unsigned long current_time = millis();
       unsigned long duration = current_time - timestamp;
       timestamp = current_time;
-      
+      */
       //Serial.println(duration);
       if(BTN_state == HIGH) {
         
         Serial.println("1");
         digitalWrite(LED_BUILTIN, HIGH);
-        buffer_array[counter] = BTN_state;
+        buffer_array[counter] = (char*)BTN_state;
       }else{
         
         Serial.println("0");
         digitalWrite(LED_BUILTIN, LOW);
-        buffer_array[counter] = BTN_state;
+        buffer_array[counter] = (char*)BTN_state;
       }
       counter++;
     }
