200d0f8055486d133bb213234d5b165ba6086d46
sadcat <lemap28@gmail.com>
2023-02-20 20:29:42 +0100

    init

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def convert_to_binary(input_data: str):
 
 def clock(sleeptime:  float):
     GPIO.output(clk, True)
+    send_first_of_stack()
     time.sleep(sleeptime)
     GPIO.output(clk, False)
     time.sleep(sleeptime)
@@ -43,19 +44,22 @@ def send(data_in):
             print(0)
         else:
             print("NaN")
-        
+
+
 def send_first_of_stack():
     queue_data = q.get()
-    bit_to_transmit = queue_data[0]
-    remaining_stack = queue_data[1:]
-    if bit_to_transmit == "1":
-        GPIO.output(TX1_pin, True)
-    elif bit_to_transmit == "0":
-        GPIO.output(TX1_pin, False)
-    else:
-        GPIO.output(TX1_pin, False)
-    q.put(remaining_stack)
+    if queue_data is not None:
+        bit_to_transmit = queue_data[0]
+        remaining_stack = queue_data[1:]
+        if bit_to_transmit == "1":
+            GPIO.output(TX1_pin, True)
+        elif bit_to_transmit == "0":
+            GPIO.output(TX1_pin, False)
+        else:
+            GPIO.output(TX1_pin, False)
+        q.put(remaining_stack)
     
+
 def pull_down():
     GPIO.output(TX1_pin, False)
 
@@ -63,16 +67,13 @@ def pull_down():
 def main():
     print(convert_to_binary(data))
     q.put(convert_to_binary(data))
-    GPIO.add_event_detect(3, GPIO.RISING, callback=send_first_of_stack)
-    GPIO.add_event_detect(3, GPIO.FALLING, callback=send_first_of_stack)
+
 
 if __name__ == "__main__":
     try:
-        #t1 = threading.Thread(target=clock, args=(0.01, 3))
-        #t2 = threading.Thread(target=send(), args=())
+        t1 = threading.Thread(target=clock, args=(0.01, 3))
         
-        #t1.start()
-        #t2.start()
+        t1.start()
         main()
     except KeyboardInterrupt:
         #GPIO.cleanup()
